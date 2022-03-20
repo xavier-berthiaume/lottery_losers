a9867e7e4892508802ba71ab1a8b87fed422f2ed
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2022-03-20 18:49:59 -0400

    Creating the class for the main window

---
+++ src/main_window.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+class Root():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Lottery Losers")
+        self.window.geometry("200x200")
+        self.window.mainloop()
