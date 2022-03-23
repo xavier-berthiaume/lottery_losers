7f6e3a53342ab9b6b581ae3159208e2fd37af893
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2022-03-23 01:46:59 -0400

    added drawing functionality, will later add cost aspect of the app, afterwards will integrate statistical functionality.

--- src/drawloop.py
+++ src/drawloop.py
@@ -1,22 +1,48 @@
+from tkinter import *
 import random
+import secrets
 import main_window
 import time
+from threading import Thread
+
 
 class DrawEngine():
 
     def __init__(self, window):
-        self.drawn_values = []
         self.winnings = 0
         self.total_cost = 0
         self.matches = [0, 0, 0, 0, 0, 0]
         self.drawing = False
         self.window = window
+        self.generateWinningCombination()
+        self.draw_thread = Thread(target = self.startDrawing)
 
 
     def startDrawing(self):
         self.drawing = True
         while self.drawing:
+            self.drawn_values = []
             for x in range(0,6):
                 self.drawn_values.append(random.randint(0, 49))
-                self.window.draw_entries[x].text = str(self.drawn_values[-1])
-                time.sleep(.1)
+                self.window.draw_entries[x].delete(0, END)
+                self.window.draw_entries[x].insert(0, str(self.drawn_values[-1]))
+            self.checkMatches()
+            #time.sleep(.1)
+
+
+    def checkMatches(self):
+        match = 0
+        for x in range(0,6):
+            if self.drawn_values[x] == self.winningCombination[x]:
+                match += 1
+        else:
+            if match > 0:
+                #the amount of matches have been calculated, now update the array indicating the number of matches and then update the gui
+                self.matches[match-1] += 1
+                self.window.match_labels[match-1].config(text = f"{match} match: {self.matches[match]}")
+
+
+    def generateWinningCombination(self):
+        self.winningCombination = []
+        for x in range(0,6):
+            self.winningCombination.append(secrets.randbelow(50))
--- src/main_window.py
+++ src/main_window.py
@@ -11,16 +11,26 @@ class Root():
         self.window = Tk()
         self.window.title("Lottery Losers")
         #self.window.geometry("200x200")
+        self.createWinningFrame()
         self.createDrawFrame()
         self.createInfoFrame()
         self.window.mainloop()
+
+
+    #create a frame that displays the winning combination of numbers right from the start
+    def createWinningFrame(self):
+        winning_frame = LabelFrame(self.window, text = "Winning Combination")
+        winning_frame.grid(row = 0, column = 0, columnspan = 2, padx = self.generic_pad_x, pady = self.generic_pad_y, ipady = 5,  sticky = E+W)
+
+        combination_label = Label(winning_frame, text = f"The winning combination: {self.draw_engine.winningCombination}")
+        combination_label.grid(row = 0, column = 0, padx = self.generic_pad_x, pady = self.generic_pad_y)
 
 
     #create the frame that contains information related to the lottery numbers that are being currently drawn
     #later on it will be necessary to access the entry fields to update the number values. The Root object's draw_entries list will contain all entry fields.
     def createDrawFrame(self):
         draw_frame = LabelFrame(self.window, text = "Lottery Draw Information")
-        draw_frame.grid(row = 0, column = 0, padx = self.generic_pad_x, pady = self.generic_pad_y)
+        draw_frame.grid(row = 1, column = 0, padx = self.generic_pad_x, pady = self.generic_pad_y)
         count = 1
         for x in range(0,12,2):
             draw_label = Label(draw_frame, text = f"Number {count}: ")
@@ -32,9 +42,11 @@ class Root():
             self.draw_entries.append(Entry(draw_frame, width = 5, justify = CENTER))
             self.draw_entries[-1].grid(row = x-1, column = 1, padx = self.generic_pad_x, pady = self.generic_pad_y)
 
-        draw_button = Button(draw_frame, text = "Start Drawing Tickets", command = lambda: self.draw_engine.startDrawing())
-        draw_button.grid(row = 100, column = 0, columnspan = 2, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = E+W)
+        draw_button = Button(draw_frame, text = "Start Drawing", command = lambda: self.draw_engine.draw_thread.start())
+        draw_button.grid(row = 100, column = 0, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = E+W)
 
+        stop_drawing_button = Button(draw_frame, text = 'Stop Drawing')
+        stop_drawing_button.grid(row = 100, column = 1, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = E+W)
 
 
     #create a frame tha contains all the information that relates to lottery wins and losses (1 match all the way to 6 match)
@@ -42,11 +54,11 @@ class Root():
     #accessing the label that displays the 'cost' of the lottery tickets is cost_label of the root object, winnings is the winnings_label
     def createInfoFrame(self):
         info_frame = LabelFrame(self.window, text = "Wins and Losses")
-        info_frame.grid(row = 0, column = 1, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = E+W+N+S)
+        info_frame.grid(row = 1, column = 1, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = E+W+N+S)
 
         self.match_labels = []
-        for x in range(1, 6):
-            self.match_labels.append(Label(info_frame, text = f"{x} match: "))
+        for x in range(0, 6):
+            self.match_labels.append(Label(info_frame, text = f"{x+1} match: "))
             self.match_labels[-1].grid(row = x, column = 0, padx = self.generic_pad_x, pady = self.generic_pad_y, sticky = W)
 
         self.cost_label = Label(info_frame, text = "Total ticket cost: $0")
